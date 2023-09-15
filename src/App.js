9d15e7efddd29d73e024124eaf3cb37a9ff785bd
Slav Mosoyan <vladanik1999@gmail.com>
2023-09-15 14:13:18 +0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import React from "react";
+import React, { Profiler } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjsScWYmyfPv3XdkNdEFVJ1wlDKMOgcSWUcg&usqp=CAU  "
-          alt=""
-        />
-      </header>
-      <nav className="nav">
-        <div>
-          <a href="">Profile</a>
-        </div>
-        <div>
-          <a href="">Message</a>
-        </div>
-        <div>
-          <a href="">News</a>
-        </div>
-        <div>
-          <a href="">Music</a>
-        </div>
-        <div>
-          <a href="">Setings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img
-            src="https://m.media-amazon.com/images/G/01/gc/designs/livepreview/amazon_dkblue_noto_email_v2016_us-main._CB468775337_.png"
-            alt=""
-          />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>ava + description</div>
-        <div>My posts</div>
-        <div>New post</div>
-        <div>
-          <div>post1</div>
-          <div>post2</div>
-        </div>
-      </div>
+      <Header />
+      <Nav />
+      <Profile />
     </div>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          className="content__image"
+          src="https://sun9-77.userapi.com/impg/tY4eGS5c8ZTLQwKFwhOlcMivqQMx1xDLQVPNDA/NqZRQo0B_Xo.jpg?size=607x1080&quality=95&sign=c620531f8e58d9de0145927323a10664&type=album"
+          alt=""
+        />
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>ava + description</div>
+      <div>My posts</div>
+      <div>New post</div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
