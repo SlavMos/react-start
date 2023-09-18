d832bae3bbbe8bfee36da5782ee3973af913f5bd
Slav Mosoyan <vladanik1999@gmail.com>
2023-09-18 13:57:44 +0400

    create messages page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="app-wrapper">
       <Header />
       <Nav />
-      <Profile />
+      <div className="app-wraper-content">
+        <Dialogs />
+      </div>
+
+      {/*<Profile />*/}
     </div>
   );
 };
