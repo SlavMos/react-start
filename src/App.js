ab773156ddf0a078776d433730ae9bfe74fc82b6
Slav Mosoyan <vladanik1999@gmail.com>
2023-09-20 11:44:11 +0400

    add Setting component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
             <Route path="Dialogs" element={<Dialogs />} />
             <Route path="News" element={<News />} />
             <Route path="Music" element={<Music />} />
+            <Route path="Settings" element={<Settings />} />
           </Routes>
         </div>
       </div>
---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./Settings.module.css";
+
+const Settings = () => {
+  return (
+    <div>
+      <p>Soon</p>
+    </div>
+  );
+};
+
+export default Settings;
